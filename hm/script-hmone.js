945194d1f108b0af2455b1221ea897c40229d684
danillevko <danillevko1212@gmail.com>
2025-06-21 10:11:37 -0600

    hm two

--- hm/script-hmone.js
+++ hm/script-hmone.js
@@ -1,13 +1,35 @@
-let age=16;
-console.log(typeof age);
-let name='Danil';
-console.log(typeof name);
-let isStudent=true;
-console.log(typeof isStudent);
-let myString='constructively/objectively';
-console.log(typeof myString);
-let myNumber=5;
-myNumber=myNumber+10;
-console.log(myNumber);
-let nyNull=null;
-console.log(typeof myNull);
+// let age=16;
+// console.log(typeof age);
+// let name='Danil';
+// console.log(typeof name);
+// let isStudent=true;
+// console.log(typeof isStudent);
+// let myString='constructively/objectively';
+// console.log(typeof myString);
+// let myNumber=5;
+// myNumber=myNumber+10;
+// console.log(myNumber);
+// let myNull=null;
+// console.log(typeof myNull);
+
+
+
+// 7
+let userName='Введіть своє ім’я';
+console.log(prompt(userName));
+let result=prompt(userName);
+alert(result);
+console.log(typeof userName);
+
+
+
+// 8
+let userAnswer='Ви дійсно хочете покинути сторінку?';
+console.log(confirm(userAnswer));
+
+
+// 9
+let aletr='Увага! Дія може бути небезпечною.';
+alert(aletr);
+let ques='Ви впевнені, що хочете продовжити?';
+console.log(confirm(ques));
--- lesson/script-variables.js
+++ lesson/script-variables.js
@@ -1,8 +1,41 @@
-let str='hi';
-console.log(typeof str);
-let num=123;
-console.log(typeof num);
-let flag=true;
-console.log(typeof flag);
-let txt='true';
-console.log(typeof txt);
+// let str='hi';
+// console.log(typeof str);
+// let num=123;
+// console.log(typeof num);
+// let flag=true;
+// console.log(typeof flag);
+// let txt='true';
+// console.log(typeof txt);
+
+
+// let one='whats ur name';
+// console.log(prompt(one));
+// let result=prompt(one);
+// console.log(result);
+
+// console.log('5'==10);
+
+// let a1=5+3;
+// console.log(a1);
+// let a2=5-3;
+// console.log(a2);
+// let a3=5*3;
+// console.log(a3);
+// let a4=5/3;
+// console.log(a4);
+
+
+// let a6=5%3;
+// alert(a6);
+// let a7=3%5;
+// alert(a7);
+// let a8=5+'3';
+// alert(a8);
+// let a9='3'-5;
+// alert(a9);
+// let a10=75+'kg';
+// alert(a10);
+
+let a='5';
+console.log(typeof Number(a));
+console.log(typeof a);
