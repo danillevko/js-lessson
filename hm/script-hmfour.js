867aa7eb1cc070a6a704ca3215c8ab61243e3e52
danillevko <danillevko1212@gmail.com>
2025-07-02 18:41:10 -0600

    hm four

---
+++ hm/script-hmfour.js
@@ -0,0 +1,54 @@
+// 1
+// let a='first';
+// let resultA=prompt(a);
+// let b='second';
+// let resultB=prompt(b);
+// const not="Не всі поля заповнені";
+// const yes="Обидва поля заповнені";
+// console.log(resultA.includes.length);
+// console.log(resultB.includes.length);
+// if(resultA || resultB === length){
+//     alert(yes);
+// }
+// else{
+//     alert(not);
+// }
+// 2
+// let a='first num';
+// let b='second num';
+// let resultA=parseInt(prompt(a));
+// let resultB=parseInt(prompt(b));
+// let sum=resultA+resultB;
+// console.log(Number(sum));
+// if(sum > 10){
+//     alert('Сума більша за 10');
+// }
+// else{
+//     alert('Сума менша або дорівнює 10');
+// }
+// 3
+// let result=prompt('string');
+// if(result.includes("JavaScript")){
+//     alert('Текст містить слово JavaScript');
+// }
+// else{
+//     alert('Текст не містить слово JavaScript');
+// }
+// 4
+// let result=parseInt(prompt('number'));
+// if(result>10 && result<20){
+//     alert('Число входить в діапазон від 10 до 20');
+// }
+// else{
+//     alert('Число не входить в діапазон від 10 до 20');
+// }
+// 5
+// let mane=prompt('name');
+// let email=prompt('email');
+// let password=parseInt(prompt('password'));
+// if(mane.length>3 && email.includes('@', '.') && password.length>6){
+// alert('Перенаправлення на іншу сторінку');
+// }
+// else{
+//     alert('Помилка: неправильне заповнення');
+// }
---
+++ lesson/script-logic.js
@@ -0,0 +1,28 @@
+// const balance = 100;
+// let message;
+
+
+// if(balance > 100){
+//    let message = "Ваш баланс позитивний!"
+//    console.log(message);
+//    const a = 5;
+// } else {
+//    let message = "Ваш баланс негативний!"
+//    console.log(message);
+//    const a = 10;
+// }
+// let message=balance > 10 ? "Ваш баланс позитивний!" : "Ваш баланс негативний!";
+// console.log(message);
+
+// let ques='Яка “офіційна” назва JavaScript?';
+// let result = prompt(ques);
+// const right = 'ECMAScript';
+// const a='Правильно';
+// const b='Ви не знаєте? ECMAScript';
+
+// if(result === right){
+//    alert(a);
+// }
+// else{
+//    alert(b);
+// }
