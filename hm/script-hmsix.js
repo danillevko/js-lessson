fef9ba5a9cc2b39480de3f86ea0aa5b3eb6fbe06
danillevko <danillevko1212@gmail.com>
2025-07-24 13:40:43 -0600

    hm six

---
+++ hm/script-hmsix.js
@@ -0,0 +1,45 @@
+// 1
+// let a=1;
+// while(a<=10){
+//     console.log(a);
+//     a++;
+// }
+
+
+// 2
+// let a=20;
+// for(i=2; i<=a; i++){
+//     if(i%2){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// 3
+// let a=10;
+// for(let i=1; i<=a; i++){
+//     console.log(7 * i);
+// }
+
+
+// 4
+// let n=100;
+// for(i=0; i<n; i++){
+//     if(i>=n){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+
+// 5
+// let a=1;
+// while(a<=20){
+//     if(a%3===0){
+//         a++;
+//         continue;
+//     }
+//     console.log(a);
+//     a++;
+// }
--- lesson/script-circle.js
+++ lesson/script-circle.js
@@ -33,13 +33,46 @@
 // }
 
 
-let a=8;
-while(true){
-    let a=prompt('what is ur grade?');
-    if(a>7){
-        console.log(a);
-    } else {
-        alert('get out');
-        break;
-    }
-}
+// let a=8;
+// while(true){
+//     let a=prompt('what is ur grade?');
+//     if(a>7){
+//         console.log(a);
+//     } else {
+//         alert('get out');
+//         break;
+//     }
+// }
+
+
+// let a;
+// do{
+// a=prompt('give me a number');
+// } while(a<100);
+// alert('thanks');
+
+
+// let a=prompt('give me a num');
+// if(Number(a)>100){
+//     alert('thanks');
+// } else{
+//     let a=prompt('give me a num');
+// }
+
+// let employees=10;
+// let totalSalary=0;
+// for(let i=0; i<employees; i++){
+//     const salary=Math.floor(Math.random()*(5000-500+1));
+//     totalSalary+=salary;
+// }
+// console.log(totalSalary);
+
+// let min=0;
+// let max=5;
+// let sum=0;
+// for(i=min; i<=max; i++){
+//     if(i%2===0){
+//         sum+=i;
+//     }
+// }
+// console.log(sum);
