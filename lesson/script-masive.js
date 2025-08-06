7275be27fca90ddea07ce80fe02ff073faa8c3da
danillevko <danillevko1212@gmail.com>
2025-08-06 12:32:18 -0600

    masives

--- lesson/script-masive.js
+++ lesson/script-masive.js
@@ -1,4 +1,4 @@
-const world = [1, 2, 3]
+// const world = [1, 2, 3]
 
 
 
@@ -30,3 +30,49 @@ const world = [1, 2, 3]
 // console.log(total);
 
 
+// const names=['bob', 'anna', 'denis'];
+// for(const item of names){
+//     console.log(item);
+// }
+
+// const Arr1=[18,19,20,21];
+// Arr1[4] = 22;
+// for(let i=0; i<=Arr1.length; i++){
+//     console.log(Arr1[i]);
+// }
+
+// const Arr2=[18,'19',20,21];
+// for(const item of Arr2){
+//     console.log(typeof item);
+//     console.log(item);
+// }
+
+// const numbers = [1, 5, 8, 9, 12, 4, 15, 27, 30, 18, 14];
+// let total = 0; 
+// for(let i=0; i<=numbers.length; i++){
+//     if(numbers[i]%2===0){
+//         total+=numbers[i];
+//     }
+// }
+// console.log(total);
+
+
+// const login=['bob', 'gabe', 'anna', 'loran'];
+// const ourLogin='danil';
+// for(let i=0; i<=login.length; i++){
+//     if(login[i] === ourLogin){
+//         console.log(`Користувач ${ourLogin} знайдено.`);
+//     } else{
+//         console.log(`Користувач ${ourLogin} не знайдено`);
+//     }
+// }
+
+
+const login=['bob', 'gabe', 'anna', 'loran'];
+const ourLogin='anna';
+for(const item of login){
+    if(item === ourLogin){
+        console.log(`Користувач ${ourLogin} знайдено.`);
+        break;
+    } 
+}
