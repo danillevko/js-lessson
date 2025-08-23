f6a300b302cacdbf661a576aaefc7c4f2e3a63bb
danillevko <danillevko1212@gmail.com>
2025-08-23 13:03:21 -0600

    call backkk

--- lesson/callback.js
+++ lesson/callback.js
@@ -14,17 +14,48 @@
 // console.log(testNums(numbers, squared));
 
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const processArray = function(numbers, callback){
-    const addedNumbers = [];
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const processArray = function(numbers, callback){
+//     const addedNumbers = [];
+//     for(let i=0; i<numbers.length; i++){
+//         addedNumbers.push(callback(numbers[i]));
+//     }
+//     return addedNumbers;
+// }
+
+// const addingFunction = function(num){
+//     return num + 1;
+// }
+
+// console.log(processArray(numbers, addingFunction));
+
+// const arr = [1, 2, 3, 4, 5];
+// let sum=0;
+// let sumArray = function(arr, callback) {
+//    for(let i=0; i<arr.length; i++){
+//     callback(arr[i]);
+//    }
+//    return sum;
+// }
+
+// const cb = function(arr){
+//     return sum += arr;
+// }
+
+// console.log(sumArray(arr, cb));
+
+
+const numbers = [1, 2, 3, 4];
+let sum=0;
+const numbersMult = (numbers, callback, a) => {
     for(let i=0; i<numbers.length; i++){
-        addedNumbers.push(callback(numbers[i]));
+        console.log(callback(numbers[i], a));
     }
-    return addedNumbers;
+    return sum;
 }
 
-const addingFunction = function(num){
-    return num + 1;
+const mult = (numbers, a) => {
+    return sum = a * numbers;
 }
 
-console.log(processArray(numbers, addingFunction));
+numbersMult(numbers, mult, 7);
